6a2fbaa80730190215aa5c725c2a4a38e1ea0a83
James Finstrom <jfinstrom@sangoma.com>
2015-10-28 12:24:50 -0700

    Slideout menu, move grid ot ajax handler

--- views/bootnav.php
+++ views/bootnav.php
@@ -1,2 +1,16 @@
-<a href="config.php?display=directory" class="list-group-item <?php echo ($request['view'] == ''? 'hidden':'')?>"><i class="fa fa-list"></i>&nbsp; <?php echo _("List Directories") ?></a>
-<a href="config.php?display=directory&view=form" class="list-group-item <?php echo ($request['view'] == 'form'? 'hidden':'')?>" ><i class="fa fa-plus"></i>&nbsp; <?php echo _("Add Directory") ?></a>
+<div id="toolbar-dirbootnav">
+	<a href="?display=directory&view=form" class="btn btn-default"><i class="fa fa-plus"></i>&nbsp;<?php echo _("Add Directory")?></a>
+  <a href="config.php?display=directory" class="btn btn-default"><i class="fa fa-list"></i>&nbsp; <?php echo _("List Directories") ?></a>
+</div>
+<table id="dirgrid" data-url="ajax.php?module=directory&command=getJSON&jdata=grid" data-cache="false" data-toggle="table" data-search="true" data-pagination="true" data-toolbar="#toolbar-dirbootnav" class="table table-striped">
+	<thead>
+			<tr>
+			<th data-field="name" data-formatter="dirlinkformatter"><?php echo _("Directory")?></th>
+		</tr>
+	</thead>
+</table>
+<script type="text/javascript">
+  function dirlinkformatter(v,r){
+    return '<a href="?display=directory&view=form&id='+r['id']+'">'+v+'</a>';
+  }
+</script>
