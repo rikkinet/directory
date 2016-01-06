a39f11d4c5149f237f1eecd22c145f6410b9a36c
Michael Newton <miken32@gmail.com>
2016-01-06 13:30:58 -0800

    escape ampersands, fix attribute

--- views/grid.php
+++ views/grid.php
@@ -3,14 +3,14 @@
 //	Copyright 2015 Sangoma Technologies.
 ?>
 <div id="toolbar-all">
-	<a href="?display=directory&view=form" class="btn btn-default"><i class="fa fa-plus"></i>&nbsp;<?php echo _("Add Directory")?></a>
+	<a href="?display=directory&amp;view=form" class="btn btn-default"><i class="fa fa-plus"></i>&nbsp;<?php echo _("Add Directory")?></a>
 </div>
 
 <table id="dirgrid"
-			data-url="ajax.php?module=directory&command=getJSON&jdata=grid"
+			data-url="ajax.php?module=directory&amp;command=getJSON&amp;jdata=grid"
 			data-cache="false"
-			data-toggle="table" d
-			ata-search="true"
+			data-toggle="table"
+			data-search="true"
 			data-pagination="true"
 			data-toolbar="#toolbar-all"
 			class="table table-striped">
