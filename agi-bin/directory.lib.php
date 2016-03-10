ea7a36011101db51f4eed3b6c78616f567ab179d
matphillips <mat+github@matphillips.me>
2016-03-10 13:30:51 +0000

    only play file (and delete it) if the file exists and the generation error code is 0. Correctly escape values

--- agi-bin/directory.lib.php
+++ agi-bin/directory.lib.php
@@ -189,15 +189,15 @@ class Dir{
 				//fallthough if not successfull
 			case 'tts':
 				// speak the name if possible, otherwise move on to spell it
-				$tmp = $this->agi_get_var('ASTSPOOLDIR') . '/tmp/directory-tts-' . time() . rand(100, 999);
-				system('flite -t "' . $con['name'] .'" -o ' . $tmp . '.wav', $exit_code);
-				if ($exit_code === 0) {
-						$ret = $this->agi->stream_file($tmp, $keys);
-						if ($ret['result']) {
-							$ret['result'] = chr($ret['result']);
-						}
-						unlink($tmp . '.wav');
-						break 2;
+				$temporaryAudioFile = $this->agi_get_var('ASTSPOOLDIR') . '/tmp/directory-tts-' . time() . rand(100, 999);
+				system('flite -t "' . escapeshellarg($con['name']) .'" -o ' . $temporaryAudioFile . '.wav', $exitCode);
+				if (file_exists($temporaryAudioFile) && $exitCode === 0) {
+					$ret = $this->agi->stream_file($temporaryAudioFile, $keys);
+					if ($ret['result']) {
+						$ret['result'] = chr($ret['result']);
+					}
+					unlink($temporaryAudioFile . '.wav');
+					break 2;
 				} else {
 					$ret = array('result' => '');
 				}
